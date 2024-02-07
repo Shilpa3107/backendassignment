c108a278e20692835d8358e5388600cef513a86d
Shilpa <shilpa.sinha3107@gmail.com>
2024-02-08 00:48:54 +0530

    Created a service

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,11 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { UserModuleModule } from './user-module/user-module.module';
 import { AuthControllerController } from './auth-controller/auth-controller.controller';
+import { AuthServiceService } from './auth-service/auth-service.service';
 
 @Module({
   imports: [UsersModule, UserModuleModule],
   controllers: [AppController, AuthControllerController],
-  providers: [AppService],
+  providers: [AppService, AuthServiceService],
 })
 export class AppModule {}
