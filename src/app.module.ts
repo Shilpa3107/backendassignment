ed7e8884448d904763dd0df295588c4658d34919
Shilpa <shilpa.sinha3107@gmail.com>
2024-02-08 00:51:50 +0530

    Created database module for handling database interactions

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { UsersModule } from './users/users.module';
 import { UserModuleModule } from './user-module/user-module.module';
 import { AuthControllerController } from './auth-controller/auth-controller.controller';
 import { AuthServiceService } from './auth-service/auth-service.service';
+import { DatabaseModuleModule } from './database-module/database-module.module';
 
 @Module({
-  imports: [UsersModule, UserModuleModule],
+  imports: [UsersModule, UserModuleModule, DatabaseModuleModule],
   controllers: [AppController, AuthControllerController],
   providers: [AppService, AuthServiceService],
 })
