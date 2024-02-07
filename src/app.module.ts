9447bbc528d794df5f447602f903f58265d6c14f
Shilpa <shilpa.sinha3107@gmail.com>
2024-02-08 00:48:01 +0530

    Created controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { UserModuleModule } from './user-module/user-module.module';
+import { AuthControllerController } from './auth-controller/auth-controller.controller';
 
 @Module({
   imports: [UsersModule, UserModuleModule],
-  controllers: [AppController],
+  controllers: [AppController, AuthControllerController],
   providers: [AppService],
 })
 export class AppModule {}
