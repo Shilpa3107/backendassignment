7bb81c7937129e0afbaa66c2929a71c70150bc2b
Shilpa <shilpa.sinha3107@gmail.com>
2024-02-08 00:54:29 +0530

    Created a User controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,11 @@ import { UserModuleModule } from './user-module/user-module.module';
 import { AuthControllerController } from './auth-controller/auth-controller.controller';
 import { AuthServiceService } from './auth-service/auth-service.service';
 import { DatabaseModuleModule } from './database-module/database-module.module';
+import { UserControllerController } from './user-controller/user-controller.controller';
 
 @Module({
   imports: [UsersModule, UserModuleModule, DatabaseModuleModule],
-  controllers: [AppController, AuthControllerController],
+  controllers: [AppController, AuthControllerController, UserControllerController],
   providers: [AppService, AuthServiceService],
 })
 export class AppModule {}
